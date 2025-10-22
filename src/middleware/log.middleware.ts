b365341cbf9dd03b30f42139af828f8fee4dd2c5
teddys48 <teddystwn48@gmail.com>
2025-10-22 19:25:04 +0700

    update

--- src/middleware/log.middleware.ts
+++ src/middleware/log.middleware.ts
@@ -3,11 +3,15 @@ import { generateRandomString } from "../helper/helper";
 
 export const LogMiddleware = () =>
   new Elysia()
-    .onBeforeHandle(async ({ request, path, store }) => {
+    .derive(async ({ request, path, store }) => {
       const body = await request.json().catch(() => null);
       console.log(`[REQ] ${path} → body:`, body);
+      return {
+        session_id: generateRandomString(),
+      };
     })
     .onAfterHandle(async ({ set, responseValue, path, store }) => {
+      // console.log(`check user  ${store}`, store);
       console.log(
         `[RES] ${path} → status: ${set.status}, response:`,
         JSON.stringify(responseValue)
