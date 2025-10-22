d724be119ed759aa078684decb59302e920cb9aa
teddys48 <teddystwn48@gmail.com>
2025-10-22 11:59:25 +0700

    update

--- src/controller/posts.controller.ts
+++ src/controller/posts.controller.ts
@@ -1,15 +1,33 @@
-import Elysia from "elysia";
+import Elysia, { t } from "elysia";
 import * as service from "../service/posts.service";
 
-export const postsController = new Elysia({ prefix: "/api" }).get(
-  "/post",
-  async function ({ set, request }) {
-    let res = await service.all(request);
-    res.code == 0
-      ? (set.status = 200)
-      : res.code == 500
-      ? (set.status = 500)
-      : (set.status = 400);
-    return res;
-  }
+export const postsController = new Elysia().group("/api", (data) =>
+  data.get(
+    "/post",
+    async ({ set, request }) => {
+      let res = await service.all(request);
+      res.code == 0
+        ? (set.status = 200)
+        : res.code == 500
+        ? (set.status = 500)
+        : (set.status = 400);
+      return res;
+    },
+    {
+      body: t.Object({
+        username: t.String(),
+      }),
+      tags: ["Posts"],
+    }
+  )
 );
+// "/post",
+// async function ({ set, request }) {
+//   let res = await service.all(request);
+//   res.code == 0
+//     ? (set.status = 200)
+//     : res.code == 500
+//     ? (set.status = 500)
+//     : (set.status = 400);
+//   return res;
+// }
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,18 @@ import { openapi } from "@elysiajs/openapi";
 import { postsController } from "./controller/posts.controller";
 
 const app = new Elysia()
-  .use(openapi())
+  .use(
+    openapi({
+      documentation: {
+        tags: [
+          {
+            name: "Posts",
+            description: "list all posts",
+          },
+        ],
+      },
+    })
+  )
   .use(postsController)
   .get("/", () => "Hello Elysia")
   .get("*", () => "what are you looking for?!")
