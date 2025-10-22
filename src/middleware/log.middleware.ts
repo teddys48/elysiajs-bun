79c39094d3861f2f864cd6e97a353489115cc035
teddys48 <teddystwn48@gmail.com>
2025-10-22 19:18:35 +0700

    update

--- src/middleware/log.middleware.ts
+++ src/middleware/log.middleware.ts
@@ -4,17 +4,10 @@ import { generateRandomString } from "../helper/helper";
 export const LogMiddleware = () =>
   new Elysia()
     .onBeforeHandle(async ({ request, path, store }) => {
-      // store.asas = generateRandomString();
-      // tunggu body jika belum parsed
-      store = { test: "" };
-      console.log("check store", store);
       const body = await request.json().catch(() => null);
       console.log(`[REQ] ${path} → body:`, body);
     })
     .onAfterHandle(async ({ set, responseValue, path, store }) => {
-      // response bisa berupa value yang returned handler
-      // let { asas } = store;
-      // console.log("store", store);
       console.log(
         `[RES] ${path} → status: ${set.status}, response:`,
         JSON.stringify(responseValue)
